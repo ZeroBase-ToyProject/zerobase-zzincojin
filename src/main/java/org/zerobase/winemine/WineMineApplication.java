ccb6cb8428934e651dcd797c9430637cf895eced
JinWoo <choiwoo2022@hanmail.net>
2024-04-29 17:42:57 +0900

    프로젝트 초기화 설정

---
+++ src/main/java/org/zerobase/winemine/WineMineApplication.java
@@ -0,0 +1,13 @@
+package org.zerobase.winemine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WineMineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WineMineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/zerobase/winemine/WinemineApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zerobase.winemine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WinemineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
